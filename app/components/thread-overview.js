09f0645adc0f963be9370fe740afb3cf136c3cf0
Madison Davis <madison.davis@my.maryvillecollege.edu>
2019-12-01 21:53:17 -0500

    Show icons on posts

--- app/components/thread-overview.js
+++ app/components/thread-overview.js
@@ -1,5 +1,24 @@
 import Component from '@ember/component';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+
+    store: service(),
+
+    init() {
+        this._super(...arguments);
+        this.setUser();
+    },
+
+    icon: null,
+
+    setUser() {
+        const store = this.get('store');
+        const user = this.get('model.owner');
+        store.find('user', user).then(foundUser => {
+            this.set('icon', foundUser.icon);
+        });
+    }
+
 });
 
