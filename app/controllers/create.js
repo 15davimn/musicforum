d019fd9eb1bc804bf964680dd6a13eb95f274797
Madison Davis <madison.davis@my.maryvillecollege.edu>
2019-12-01 22:47:18 -0500

    Persist posts

--- app/controllers/create.js
+++ app/controllers/create.js
@@ -1,12 +1,30 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
     queryParams: ['thread'],
+
     thread: null,
+
+    user: localStorage.user,
+
+    store: service(),
 
     actions: {
         submit() {
-            console.log(this.get('thread'));
+            const thread = this.get('thread');
+            const post = this.get('post');
+            const user = this.get('user');
+            const store = this.get('store');
+            const newPost = store.createRecord('post', {
+                id: 88,
+                thread: thread,
+                content: post,
+                user: user
+            });
+            newPost.save().catch(error => {
+                //window.location.replace('/');
+            });
         }
     }
 });
